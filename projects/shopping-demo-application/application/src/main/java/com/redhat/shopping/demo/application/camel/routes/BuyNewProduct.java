ecf31f446d2af53c8b68b72518a1a254a75ba1d3
Ankit Verma <ankit.b.verma@accenture.com>
2015-02-27 02:41:23 +0530

    Added record transaction feature

--- projects/shopping-demo-application/application/src/main/java/com/redhat/shopping/demo/application/camel/routes/BuyNewProduct.java
+++ projects/shopping-demo-application/application/src/main/java/com/redhat/shopping/demo/application/camel/routes/BuyNewProduct.java
@@ -1,29 +1,21 @@
 package com.redhat.shopping.demo.application.camel.routes;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.apache.camel.ValidationException;
 import org.apache.camel.builder.RouteBuilder;
 
 public class BuyNewProduct extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		onException(ValidationException.class).handled(true)
+		.transform().simple("The user details are missing in the request.");
 		from("vm:buyProductsByCode")
 		.log("Buy Request Started")
-		.process(new Processor() {
-			
-			public void process(Exchange exchange) throws Exception {
-				exchange.getIn().setHeader("awaitingResponse",
-						true);				
-			}
-		})
+		.log("${body}")
 		.setHeader("productCode",simple("${body[0]}"))
+		.validate().simple("${body[1]} != null")
+		.setHeader("customerDetails",simple("${body[1]}"))
 		.to("activemq:buyProductsByCode")
 		.transform(constant("Thank you for buying the products"));
 	}
-	
-	
-	
-	
-
 }
