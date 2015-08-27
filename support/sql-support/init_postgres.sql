ae38d83cd669758d437d1193aea42ec0dab60cce
Ankit Verma <ankit.b.verma@accenture.com>
2015-08-27 16:42:36 +0530

    Updated Postgres sql support

--- support/sql-support/init_postgres.sql
+++ support/sql-support/init_postgres.sql
@@ -3,9 +3,4 @@ CREATE TABLE transactiondb.transactionHistory (
 transactionId SERIAL PRIMARY KEY,
 customerDetails text NOT NULL,
 productCode text NOT NULL,
-dateOfPurchase date NOT NULL);CREATE SCHEMA IF NOT EXISTS transactionDB;
-CREATE TABLE transactiondb.transactionHistory (
-transactionId SERIAL PRIMARY KEY,
-customerDetails text NOT NULL,
-productCode text NOT NULL,
 dateOfPurchase date NOT NULL);
